2118f24cd7e1b6b36bcb02decaf1cd36345a742c
A721326 <71582320+A721326@users.noreply.github.com>
2022-11-20 06:50:36 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{useState} from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+  
+ 
+
+  return (
+    <div className="App">
+      <h1>Counter app</h1>
+
+      <div className="container">
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(+e.target.value)}
+          placeholder="0"
+        />
+        
+      </div>
+
+      <button onClick={()=>setCount(count+1)}>Increase </button>
+      <button onClick={()=>setCount(count-1)}>Increase </button>
+
+      <h2>{count}</h2>
+    </div>
+  );
+}
+
+
+export default App;
