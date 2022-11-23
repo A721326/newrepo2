9628139d0f71277c0fa66dfaaa3e82e42d506fdf
A721326 <71582320+A721326@users.noreply.github.com>
2022-11-22 23:12:38 -0800

    i add counter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 import React,{useState} from 'react';
-
+import Button from"./components/Button";
 function App() {
   const [count, setCount] = useState(0);
-  
- 
+     return (
+     <div className="container">
 
-  return (
-    <div className="App">
-      <h1>Counter app</h1>
+         <div className="main-div">
+           <div className='center-div'>
+             <h1>counter app</h1>
+               
+                    
+                   <input type="text" value={count} onChange={(e) => setCount(+e.target.value)} placeholder="0" /><br/>
+                   <h2>{count}</h2>
+                 
+                  
+                   
+               <div className='row'>
+               
+               <button  onClick={()=>setCount(count+1)}>Increase </button>
+               <button onClick={()=>setCount(count-1)}>Decrease </button>
+               <button  onClick={()=>setCount(0)}>reset</button><br/><br/><br/>
 
-      <div className="container">
-        <input
-          type="text" value={count} onChange={(e) => setCount(+e.target.value)} />
-        
-      </div>
 
-      <button onClick={()=>setCount(count+1)}>Increase </button>
-      <button onClick={()=>setCount(count-1)}>Increase </button>
-      <button onClick={()=>setCount(0)}>reset</button>
-      <h2>{count}</h2>
+               <input type="text" value={count} onChange={(e) => setCount(+e.target.value)} placeholder="0" /><br/><br/>
+                  <h2>{count}</h2>
+                 
+                  
+                   
+               <div className='row1'>
+               
+               <button  onClick={()=>setCount(count+1)}>Increase </button>
+               <button onClick={()=>setCount(count-1)}>Decrease </button>
+               <button onClick={()=>setCount(0)}>reset</button><br/><br/><br/>
+               
+               
+               <input type="text" value={count} onChange={(e) => setCount(+e.target.value)} placeholder="0" /><br/><br/>
+                  <h2>{count}</h2>
+                 
+                  
+                   
+               <div className='row1'>
+               
+               <button  onClick={()=>setCount(count+1)}>Increase </button>
+               <button onClick={()=>setCount(count-1)}>Decrease </button>
+               <button onClick={()=>setCount(0)}>reset</button>
+               
+
+             </div>
+           </div>
     </div>
+    
+    </div> 
+</div>
+</div>
+ 
   );
 }
 
